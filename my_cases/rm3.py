05e2190cb4ce5f1811183717008f46cb3376a41a
akeeste <akeeste@sandia.gov>
2020-10-19 14:54:00 -0500

    udpate

--- my_cases/rm3.py
+++ my_cases/rm3.py
@@ -17,47 +17,75 @@ import os
 
 logging.basicConfig(level=logging.WARNING)
 
-# create buoy (float of the WEC, but 'float' is reserved in Python)
-buoy_file = os.getcwd() + os.path.sep + 'float_ref.stl'
-# buoy_file = os.getcwd() + os.path.sep + 'float.stl'
-buoy = cpt.FloatingBody.from_file(buoy_file)
+# create buoy from input 
+# (float of the WEC, but 'float' is reserved in Python)
+# buoy_file = os.getcwd() + os.path.sep + 'float_ref.stl'
+
+buoy_file = os.getcwd() + os.path.sep + 'Meshes\\nemoh_float.dat'
+form = 'nemoh'
+
+# buoy_file = os.getcwd() + os.path.sep + 'Meshes\\wamit_float.gdp'
+# form = 'wamit'
+
+buoy = cpt.FloatingBody.from_file(
+    filename=buoy_file,
+    file_format='stl',
+    name='buoy')
+buoy.translate_z(-0.72)
+buoy.keep_immersed_part()
 buoy.add_translation_dof(name="Surge")
 buoy.add_translation_dof(name="Sway")
 buoy.add_translation_dof(name="Heave")
 buoy.add_rotation_dof(name="Roll")
 buoy.add_rotation_dof(name="Pitch")
 buoy.add_rotation_dof(name="Yaw")
-buoy.translate_z(-0.72)
-buoy.keep_immersed_part()
 
-# create plate from input stl file
-plate_file = os.getcwd() + os.path.sep + 'plate_ref.stl'
-# plate_file = os.getcwd() + os.path.sep + 'plate.stl'
-plate = cpt.FloatingBody.from_file(plate_file)
+
+# create plate from input file
+# plate_file = os.getcwd() + os.path.sep + 'plate_ref.stl'
+# form = 'stl'
+
+plate_file = os.getcwd() + os.path.sep + 'Meshes\\nemoh_spar.dat'
+form = 'nemoh'
+
+# plate_file = os.getcwd() + os.path.sep + 'Meshes\\wamit_spar.gdp'
+# form = 'wamit'
+
+plate = cpt.FloatingBody.from_file(
+    filename=plate_file,
+    file_format=form,
+    name='plate')
+plate.translate_z(-21.29)
+plate.keep_immersed_part()
 plate.add_translation_dof(name="Surge")
 plate.add_translation_dof(name="Sway")
 plate.add_translation_dof(name="Heave")
 plate.add_rotation_dof(name="Roll")
 plate.add_rotation_dof(name="Pitch")
 plate.add_rotation_dof(name="Yaw")
-plate.translate_z(-21.29)
-plate.keep_immersed_part()
 
 combo = plate+buoy
 # combo.show()
+###################################################################
 
 all_dofs = ['Surge','Sway','Heave','Roll','Pitch','Yaw']
 
 
 # Note: radiating_dof can only include up to the body's dofs 
 test_matrix = xr.Dataset(coords={
-    # 'omega': np.linspace(0.02, 5.2, 260),
-    'omega': np.linspace(0.02, 5.2, 5),
+    'omega': np.linspace(0.02, 5.2, 10),
+    # 'omega': np.linspace(0.02, 5.2, 5),
     'wave_direction': np.linspace(0, np.pi/2, 2),
     'theta': np.linspace(0, np.pi/2, 2),
+    
     'influenced_dof': all_dofs,
-    # 'radiating_dof': list(body.dofs),  
-    'radiating_dof': all_dofs,
+    # 'influenced_dof': list(combo.dofs),
+    # 'influenced_dof': combo.dofs,
+     
+    # 'radiating_dof': all_dofs,
+    # 'radiating_dof': list(combo.dofs),
+    'radiating_dof': combo.dofs,
+    
     'water_depth': [np.infty],
 })
 
@@ -67,10 +95,11 @@ solver = cpt.BEMSolver()
 
 rm3_results = solver.fill_dataset(
     test_matrix, 
-    [buoy], 
+    # [buoy, plate],
+    [comob],
     wavenumber=True, 
     wavelength=True,
-    mesh=False, 
+    mesh=True, 
     hydrostatics=True,
     # keep_details=True
     )
@@ -81,4 +110,3 @@ rm3_results = cpt.io.xarray.separate_complex_values(rm3_results)
 filename = os.getcwd() + os.path.sep + 'rm3.nc'
 print(filename)
 rm3_results.to_netcdf(filename)
-
